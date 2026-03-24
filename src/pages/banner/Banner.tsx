1c69e4148d56eb462ba726f0fddac5b0cb713213
bigphu <bigphu2005@gmail.com>
2026-03-24 23:18:58 +0700

    added StatBox to Banner

--- src/pages/banner/Banner.tsx
+++ src/pages/banner/Banner.tsx
@@ -1,25 +1,34 @@
 import type { JSX } from "react";
+import { StatBox } from "@/components";
 import "./Banner.css";
 
 const Banner = (): JSX.Element => {
   return (
     <section className="full-screen-section">
-      <div className="container">
-        <h1>
-          <span className="gradient-title">FRONTEND</span>
-          <br></br>
-          <span className="developer">DEVELOPER</span>
-        </h1>
+      <div className="container banner-layout">
+        <div className="banner-content">
+          <h1>
+            <span className="gradient-title gradient-text">FRONTEND</span>
+            <br></br>
+            <span className="developer">DEVELOPER</span>
+          </h1>
 
-        <p className="banner-description">
-            Hi! I&apos;m{' '}
-            <span className="my-name">
-                Phạm Trần Gia Phú
-            </span>
-            . An undergraduate Frontend Developer that 
-            has been through building scalable and
-            responsive web projects.
-        </p>
+          <p className="banner-description">
+              Hi! I&apos;m{' '}
+              <span className="my-name">
+                  Phạm Trần Gia Phú
+              </span>
+              . 
+              <br></br>
+              An undergraduate Frontend Developer that 
+              has been through building scalable and
+              responsive web projects.
+          </p>
+        </div>
+
+        <div className="banner-stat">
+          <StatBox items={ [{ desc: "CGPA", stat: "3.5" }, { desc: "IETLS", stat: "7.5" }] }/>
+        </div>
       </div>
     </section>
   )
